8a684bad84ae8772ac0905edbe92e2fc10e2d803
Don Chaney <dac5197@live.com>
2021-03-17 10:12:49 -0400

    Implement charge create functionality

--- BatteryApp/wwwroot/js/bootstrapInteropt.js
+++ BatteryApp/wwwroot/js/bootstrapInteropt.js
@@ -1,8 +1,15 @@
-﻿window.bootstrapInteropt = {
-    showModal: id => {
-        $(`#${id}`).modal('show');
-    },
-    closeModal: id => {
-        $(`#${id}`).modal('hide');
-    }
-};
+﻿////window.bootstrapInteropt = {
+////    showModal: id => {
+////        $(`#${id}`).modal('show');
+////    },
+////    closeModal: id => {
+////        $(`#${id}`).modal('hide');
+////    }
+////};
+
+function ShowModal(modalId) {
+    $('#' + modalId).modal('show');
+}
+function CloseModal(modalId) {
+    $('#' + modalId).modal('hide');
+}
