a25b5ac42522efe2cc618f2783199764fc6ed9b3
Don Chaney <dac5197@live.com>
2021-06-01 16:37:47 -0400

    Configured dependencies

--- BatteryApp/Program.cs
+++ BatteryApp/Program.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace BatteryApp
 {
@@ -25,6 +26,10 @@ namespace BatteryApp
                     {
                         config.ConfigureKeyVault();
                     }
+                    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                    config.AddAzureKeyVault(
+                    keyVaultEndpoint,
+                    new DefaultAzureCredential());
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
